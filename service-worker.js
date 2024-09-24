ba43183333dd77b7beb7cc37de3e9dffb7f4231d
Luiz Felipe <60053677+LuizFelipeP@users.noreply.github.com>
2024-09-24 01:35:29 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,15 @@
 const cacheName = 'pwa-cache-v1';
 const assetsToCache = [
-  '/',
-  '/index.html',
-  '/add-expense.html',
-  '/dashboard.html',
-  '/add-expense.js',
-  '/script.js',
-  '/dashboard.js',
-  '/style.css',
-  '/service-worker.js',
-  '/editar.js',
-  '/editar.html'
+  'index.html',
+  'add-expense.html',
+  'dashboard.html',
+  'add-expense.js',
+  'script.js',
+  'dashboard.js',
+  'style.css',
+  'service-worker.js',
+  'editar.js',
+  'editar.html'
 
 
   // Adicione aqui todos os arquivos que deseja manter em cache
